331785f5e231708853f8d6a1d6a42fb7a1c0e52c
ck <70942824+ck-pp@users.noreply.github.com>
2024-05-01 02:30:25 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -0,0 +1,16 @@
+def solution(skill, skill_trees):
+    ans = 0
+    for sk in skill_trees:
+        tf = 1
+        st = list(skill)
+        for s in sk:
+            if s in st:
+                # print(s, st)
+                if s != st[0]:
+                    tf = 0
+                    break
+                else:
+                    st.pop(0)
+        if tf:
+            ans += 1
+    return ans
