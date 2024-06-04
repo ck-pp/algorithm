d3e12a5bfbae63a33ee46c7c4ed090ce837cef6a
ck <70942824+ck-pp@users.noreply.github.com>
2024-06-05 03:05:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.01 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 5.09 ms
+메모리: 10.5 MB, 시간: 5.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 05일 03:03:57
+2024년 06월 05일 03:05:34
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -2,21 +2,29 @@ from collections import deque
 
 def solution(maps):
     n, m = len(maps), len(maps[0])
-    queue = deque([((0, 0), 1)]) # xy좌표, 이동 칸 수 1부터 시작(시작지점도 포함이므로!)
-    visited = set([(0, 0)])
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 상, 하, 좌, 우 방향
+    queue = deque([((1, 1), 1)]) # xy좌표, 이동 칸 수 1부터 시작 (시작지점도 포함이므로!)
+    visited = set([(1, 1)])
     
     while queue:
-        (cur_x, cur_y), steps = queue.popleft()
+        cur_xy, steps = queue.popleft()
+        cur_x, cur_y = cur_xy
 
-        if (cur_x, cur_y) == (n-1, m-1):
+        if cur_xy == (n, m):
             return steps
+        
+        c_xy = []
+        if cur_x > 1:
+            c_xy.append((cur_x-1, cur_y))
+        if cur_x < n:
+            c_xy.append((cur_x+1, cur_y))
+        if cur_y > 1:
+            c_xy.append((cur_x, cur_y-1))
+        if cur_y < m:
+            c_xy.append((cur_x, cur_y+1))
             
-        for dx, dy in directions:
-            next_x, next_y = cur_x + dx, cur_y + dy
-            
-            if (0 <= next_x < n and 0 <= next_y < m) and maps[next_x][next_y] == 1 and (next_x, next_y) not in visited:
-                visited.add((next_x, next_y))
-                queue.append(((next_x, next_y), steps + 1))
+        for next_xy in c_xy:
+            if maps[next_xy[0]-1][next_xy[1]-1] != 0 and next_xy not in visited:
+                visited.add(next_xy)
+                queue.append((next_xy, steps + 1))
                 
     return -1
