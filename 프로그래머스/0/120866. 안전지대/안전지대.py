03dceb71afceccf00f7f150d9cc7813e0a0fb65e
ck <70942824+ck-pp@users.noreply.github.com>
2024-06-06 21:34:04 +0900

    [level 0] Title: 안전지대, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+def solution(board):
+    sum = 0
+    n = len(board)
+    dp = [[0 for _ in board] for _ in board]
+    directions = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+    
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 1:
+                dp[i][j] = 1
+                for dx, dy in directions:
+                    next_x, next_y = i+dx, j+dy
+                    if (-1 < next_x < n and -1 < next_y < n) and dp[next_x][next_y] == 0:
+                        dp[next_x][next_y] = 1
+    for r in range(n):
+        sum += Counter(dp[r])[0]
+        
+    return sum
