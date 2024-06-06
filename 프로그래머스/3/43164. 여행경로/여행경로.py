f3c31f9709b492a66d75854e0a46cf7ad930f20f
ck <70942824+ck-pp@users.noreply.github.com>
2024-06-07 03:34:28 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+def solution(tickets):
+    route = []
+    # 그래프 생성
+    graph = defaultdict(list)
+    
+    for start, end in tickets:
+        graph[start].append(end)
+    
+    # 도착지 사전순 정렬 _ 제한사항 조건 충족 위해
+    for key in graph:
+        graph[key].sort(reverse=True) # pop -> 역순 정렬
+        
+    def dfs(node):
+        while graph[node]:
+            next_node = graph[node].pop()
+            dfs(next_node)
+        route.append(node)
+    
+    dfs("ICN")    
+    
+    return route[::-1] # 재귀 > 방문 경로 역순으로 저장 > 뒤집어서 반환
