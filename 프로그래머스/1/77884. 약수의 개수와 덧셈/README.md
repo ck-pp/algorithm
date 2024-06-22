0622668d2beb0ab608d8e791680fb00c4d7b3ffe
ck <70942824+ck-pp@users.noreply.github.com>
2024-06-23 04:00:56 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.11 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 개수와 덧셈 - 77884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.30 ms
+메모리: 33.3 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 23일 03:51:36
+2024년 06월 23일 04:00:55
 
 ### 문제 설명
 
--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,15 +1,10 @@
 function solution(left, right) {
     var ans = 0;
     for (let i = left; i < right+1; i++) {
-        var num = 0;
-        for (let j = 1; j < i+1; j++) {
-            if (i % j == 0)
-                num += 1
-        }
-        if (num % 2 == 0)
-            ans += i
-        else
+        if (Math.sqrt(i) % 1 == 0)
             ans -= i
-             }
+        else
+            ans += i
+    }
     return ans;
 }
