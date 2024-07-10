3476d6c29bac6e0f8ab24fad51c7aa2e9a206027
ck <70942824+ck-pp@users.noreply.github.com>
2024-07-11 02:58:00 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/README.md
+++ 프로그래머스/2/12911. 다음 큰 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다음 큰 숫자 - 12911 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12911?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12911) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.04 ms
+메모리: 33.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 11일 02:55:51
+2024년 07월 11일 02:57:59
 
 ### 문제 설명
 
--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,11 +1,13 @@
 function solution(n) {
-    var n_next = n + 1;
-    var n_bin_1 = n.toString(2).replaceAll('0', '').length;
-    while (true) {
-        var n_next_bin_1 = n_next.toString(2).replaceAll('0', '').length;
-        if (n_next_bin_1 == n_bin_1)
-            return n_next;
-        else
-            n_next += 1;
-    }
+    var i, j;
+    // 1을 찾을때까지 우로 쉬프트, 쉬프트 횟수 = i
+    for (i = 0; !(n & 1); i++) {n = n >> 1; } 
+    // 0을 찾을때까지 우로 쉬프트, 1의 갯수 = j
+    for (j = 0; n & 1; i++, j++) {n = n >> 1; } 
+    // 0자리에 1대입, 1의 갯수 -1, i === j 가 될때까지 죄로 쉬프트하면서 쉬프트 횟수 -1
+    for (j--, n++; i !== j; i--) {n = n << 1; } 
+    // i === 0 될때까지 좌로 쉬프트 하면서 쉬프트 횟수 -1, 0자리에 1대입
+    for (i; i; i--, n++) {n = n << 1; } 
+    
+    return n;
 }
