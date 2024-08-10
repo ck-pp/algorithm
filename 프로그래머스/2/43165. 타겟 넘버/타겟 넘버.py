8e738d14b524839ff22cf0006549e1ea66eed35f
ck <70942824+ck-pp@users.noreply.github.com>
2024-08-10 18:55:28 +0900

    [level 2] Title: 타겟 넘버, Time: 388.28 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,7 +1,7 @@
 def solution(numbers, target):
-    def dfs(idx, cur_sum):
+    def dfs(idx, c_sum):
         if idx == len(numbers):
-            return 1 if cur_sum == target else 0
-        return dfs(idx + 1, cur_sum + numbers[idx]) + dfs(idx + 1, cur_sum - numbers[idx])
+            return 1 if c_sum == target else 0
+        return dfs(idx + 1, c_sum + numbers[idx]) + dfs(idx + 1, c_sum - numbers[idx])
     
     return dfs(0, 0)
