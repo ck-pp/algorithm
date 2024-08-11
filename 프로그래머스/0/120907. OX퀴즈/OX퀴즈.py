84dfe521e5cc4dd6770e6b45f815a2faa2b3795e
ck <70942824+ck-pp@users.noreply.github.com>
2024-08-12 04:03:32 +0900

    [level 0] Title: OX퀴즈, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -2,13 +2,11 @@ def solution(quiz):
     ans = []
     for q in quiz:
         num1, op, num2, eq, res = q.split(' ')
-        right = "X"
         if op == '-':
-            if int(num1) - int(num2) == int(res):
-                right = "O"
+            right = "O" if int(num1) - int(num2) == int(res) else "X"
+            ans.append(right)
         else:
-            if int(num1) + int(num2) == int(res):
-                right = "O"
-        ans.append(right)
+            right = "O" if int(num1) + int(num2) == int(res) else "X"
+            ans.append(right)
         
     return ans
