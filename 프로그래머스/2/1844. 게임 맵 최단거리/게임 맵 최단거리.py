7182a917a280a9704986ff07efaf28cc93c67dc7
ck <70942824+ck-pp@users.noreply.github.com>
2024-08-14 02:05:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.97 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,30 +1,21 @@
 from collections import deque
 
 def solution(maps):
+    queue = deque([(1, (1, 1))]) # 이동 칸 수, 현재 좌표
+    visited = set([(1, 1)]) # 좌표
+    d_pos = [(1, 0), (-1, 0), (0, -1), (0, 1)]
     n, m = len(maps), len(maps[0])
-    queue = deque([((1, 1), 1)]) # xy좌표, 이동 칸 수 1부터 시작 (시작지점도 포함이므로!)
-    visited = set([(1, 1)])
     
     while queue:
-        cur_xy, steps = queue.popleft()
-        cur_x, cur_y = cur_xy
-
-        if cur_xy == (n, m):
+        steps, cur_pos = queue.popleft()
+        
+        if cur_pos == (n, m):
             return steps
         
-        c_xy = []
-        if cur_x > 1:
-            c_xy.append((cur_x-1, cur_y))
-        if cur_x < n:
-            c_xy.append((cur_x+1, cur_y))
-        if cur_y > 1:
-            c_xy.append((cur_x, cur_y-1))
-        if cur_y < m:
-            c_xy.append((cur_x, cur_y+1))
-            
-        for next_xy in c_xy:
-            if maps[next_xy[0]-1][next_xy[1]-1] != 0 and next_xy not in visited:
-                visited.add(next_xy)
-                queue.append((next_xy, steps + 1))
-                
+        for dx, dy in d_pos:
+            next_x, next_y = dx + cur_pos[0], dy + cur_pos[1]
+            if (1 <= next_x <= n) and (1 <= next_y <= m) and (next_x, next_y) not in visited and maps[next_x-1][next_y-1] == 1:        
+                queue.append((steps + 1, (next_x, next_y)))
+                visited.add((next_x, next_y))
+        
     return -1
