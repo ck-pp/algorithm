39e7dfbb54b147a45579c9d97b0b3aef17b40aa4
ck <70942824+ck-pp@users.noreply.github.com>
2024-08-19 13:14:37 +0900

    [level 0] Title: 그림 확대, Time: 0.24 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.py
@@ -1,7 +1,6 @@
 def solution(picture, k):
     ans = []
     for p in picture:
-        p = p.replace('.', '.' * k).replace('x', 'x' * k)
         for _ in range(k):
-            ans.append(p)
+            ans.append(p.replace('.', '.' * k).replace('x', 'x' * k))
     return ans
