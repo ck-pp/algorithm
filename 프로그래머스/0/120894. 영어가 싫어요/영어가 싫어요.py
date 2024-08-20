32fbac652c103e95f3ce7c21623b6f3dfa1b004f
ck <70942824+ck-pp@users.noreply.github.com>
2024-08-21 01:12:47 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,6 +1,5 @@
 def solution(numbers):
-    eng_num = {"zero": "0","one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
-    for eng in eng_num.keys():
-        numbers = numbers.replace(eng, eng_num[eng])
+    for idx, eng in enumerate(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+        numbers = numbers.replace(eng, str(idx))
         
     return int(numbers)
