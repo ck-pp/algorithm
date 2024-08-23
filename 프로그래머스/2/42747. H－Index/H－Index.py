5d7e9c037e8746c338e07010b339845d7dac31cf
ck <70942824+ck-pp@users.noreply.github.com>
2024-08-23 22:30:06 +0900

    [level 2] Title: H-Index, Time: 2.76 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,32 +1,16 @@
-def merge_sort(arr): # 반으로 나누어 재귀적으로 정렬
+def quick_sort(arr):
     if len(arr) <= 1:
         return arr
     
-    mid = len(arr) // 2
-    left = merge_sort(arr[:mid])
-    right = merge_sort(arr[mid:])
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x > pivot] # 부등호에 따라 내림차순/오름차순 정렬할 수 있다.
+    equal = [x for x in arr if x == pivot]
+    right = [x for x in arr if x < pivot]
     
-    return merge(left, right)
-
-def merge(left, right): # 병합
-    res = []
-    i = j = 0
-    
-    while i < len(left) and j < len(right):
-        if (left[i] > right[j]):
-            res.append(left[i])
-            i += 1
-        else:
-            res.append(right[j])
-            j += 1
-        
-    res.extend(left[i:])
-    res.extend(right[j:])
-        
-    return res
+    return quick_sort(left) + equal + quick_sort(right)
 
 def solution(citations):
-    sorted_citations = merge_sort(citations) # 병합 정렬(내림차순)
+    sorted_citations = quick_sort(citations) # 퀵 정렬(내림차순)
     h_idx = 0
     for i, citation in enumerate(sorted_citations):
         if citation >= i + 1:
@@ -35,4 +19,3 @@ def solution(citations):
             break
         
     return h_idx
-    
