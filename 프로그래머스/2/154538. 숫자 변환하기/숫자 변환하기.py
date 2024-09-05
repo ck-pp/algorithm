e713584d04fc26be0007cbceb600c752a77f18ba
ck <70942824+ck-pp@users.noreply.github.com>
2024-09-05 21:31:47 +0900

    [level 2] Title: 숫자 변환하기, Time: 146.80 ms, Memory: 28.8 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,27 +1,26 @@
 from collections import deque
 
 def solution(x, y, n):
+    q = deque([(y, 0)])  # (값, 연산 횟수)
     
-    queue = deque([[0, y]])
-    visited = set([y])
+    if y == x:
+        return 0
     
-    while queue:
-        cnt, cur_sum = queue.popleft()
+    while q:
+        cur_val, steps = q.popleft()
         
-        if cur_sum == x:
-            return cnt
+        if cur_val == x:
+            return steps
         
-        num_list = []
-        if cur_sum - n >= x:
-            num_list.append(cur_sum - n)
-        if cur_sum % 2 == 0:
-            num_list.append(cur_sum // 2)
-        if cur_sum % 3 == 0:
-            num_list.append(cur_sum // 3)
-            
-        for next_num in num_list:
-            if next_num not in visited:
-                queue.append([cnt + 1, next_num])
-                visited.add(next_num)
-    
+        val_list = []
+        if cur_val - n > 0:
+            val_list.append(cur_val - n)
+        if cur_val % 2 == 0:
+            val_list.append(cur_val // 2)
+        if cur_val % 3 == 0:
+            val_list.append(cur_val // 3)
+        
+        for next_val in val_list:
+            q.append((next_val, steps + 1))
+
     return -1
