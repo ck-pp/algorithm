848dd23bf84557b6d53a91663c239d9c7325df70
ck <70942824+ck-pp@users.noreply.github.com>
2024-09-07 23:28:24 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.21 ms
+메모리: 10 MB, 시간: 0.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 13일 22:44:37
+2024년 09월 07일 23:28:22
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,16 +1,16 @@
 def solution(n, computers):
-    def dfs(node):
+    def dfs(node, visited):
         visited[node] = True
         for neighbor in range(n):
-            if computers[node][neighbor] == 1 and not visited[neighbor]:
-                dfs(neighbor)
-    
+            if computers[node][neighbor] == 1 and visited[neighbor] == False:
+                dfs(neighbor, visited)
+        
     visited = [False] * n
-    network_cnt = 0
+    net_cnt = 0
     
     for i in range(n):
         if not visited[i]:
-            dfs(i)
-            network_cnt += 1
+            dfs(i, visited)
+            net_cnt += 1
             
-    return network_cnt
+    return net_cnt
