e1ef2adc442ca69a702dd67e260a45495661401a
ck <70942824+ck-pp@users.noreply.github.com>
2024-09-09 18:21:14 +0900

    [level 3] Title: 가장 먼 노드, Time: 26.71 ms, Memory: 20.3 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,23 +1,26 @@
+# 최단경로 -> BFS / 다익스트라
+# 가중치 X 이므로, BFS 사용
+
 from collections import deque
 
 def solution(n, vertex):
-    # 1번 노드로부터 ~ : 다익스트라인줄 알았는데 BFS가 더 효율적이다. 가중치가 없기 때문에.
+    
     g = [[] for _ in range(n + 1)]
     
-    for a, b in vertex:
-        g[a].append(b)
-        g[b].append(a)
+    distance = [-1] * (n + 1)  # 이걸로 방문 여부 파악, -1은 아직 방문하지 않았다는 의미
+    distance[1] = 0  # 1 -> 1은 0이므로
+    q = deque([1])  # (노드)
     
-    distance = [-1] * (n + 1)  # 모든 노드 거리 -1로 초기화
-    distance[1] = 0
-    q = deque([1])  # 1번 노드가 시작점
-    
+    for start, end in vertex:
+        g[start].append(end)
+        g[end].append(start)
+        
     while q:
         cur_node = q.popleft()
-        
+         
         for neighbor in g[cur_node]:
-            if distance[neighbor] == -1:  # 아직 방문하지 않은 노드라면
-                distance[neighbor] = distance[cur_node] + 1
+            if distance[neighbor] == -1:
                 q.append(neighbor)
+                distance[neighbor] = distance[cur_node] + 1
     
     return distance.count(max(distance))
