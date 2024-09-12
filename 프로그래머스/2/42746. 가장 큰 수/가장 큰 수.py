3ac293679d7abd648a3a4d8447dd074fb553faff
ck <70942824+ck-pp@users.noreply.github.com>
2024-09-13 02:01:43 +0900

    [level 2] Title: 가장 큰 수, Time: 1520.43 ms, Memory: 22.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -3,15 +3,20 @@ def quick_sort(arr):
         return arr
     
     pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x + pivot > pivot + x]
-    equal = [x for x in arr if x + pivot == pivot + x]
-    right = [x for x in arr if x + pivot < pivot + x]
+    left, equal, right = [], [], []
     
+    for num in arr:
+        if num + pivot > pivot + num:
+            left.append(num)
+        elif num + pivot < pivot + num:
+            right.append(num)
+        else:
+            equal.append(num)
+            
     return quick_sort(left) + equal + quick_sort(right)
 
 def solution(numbers):
     numbers = list(map(str, numbers))
-    sorted_numbers = quick_sort(numbers)
-    ans = ''.join(sorted_numbers)
+    sorted_numbers = ''.join(quick_sort(numbers))
     
-    return str(int(ans))
+    return str(int(sorted_numbers))  # 맨앞? 0이 (여러개) 붙을 경우
