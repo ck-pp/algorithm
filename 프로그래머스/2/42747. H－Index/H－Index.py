3d7b356b287bfab57ac9c57290c158298a00484b
ck <70942824+ck-pp@users.noreply.github.com>
2024-09-16 12:03:52 +0900

    [level 2] Title: H-Index, Time: 1.31 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -3,17 +3,24 @@ def quick_sort(arr):
         return arr
     
     pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x > pivot] # 부등호에 따라 내림차순/오름차순 정렬할 수 있다.
-    equal = [x for x in arr if x == pivot]
-    right = [x for x in arr if x < pivot]
+    left, equal, right = [], [], []
     
+    for num in arr:
+        if num > pivot:  # 내림차순 정렬
+            left.append(num)
+        elif num < pivot:
+            right.append(num)
+        else:
+            equal.append(num)
+            
     return quick_sort(left) + equal + quick_sort(right)
 
 def solution(citations):
-    sorted_citations = quick_sort(citations) # 퀵 정렬(내림차순)
+    sorted_citations = quick_sort(citations)
     h_idx = 0
-    for i, citation in enumerate(sorted_citations):
-        if citation >= i + 1:
+    
+    for i, c in enumerate(sorted_citations):
+        if c >= i + 1:  # i가 배열 인덱스이므로 +1
             h_idx = i + 1
         else:
             break
