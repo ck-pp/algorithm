c786261e132332a881d43add25dc97652ca58548
ck <70942824+ck-pp@users.noreply.github.com>
2024-09-20 22:37:05 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.py
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.py
@@ -1,2 +1,20 @@
-def solution(num_list):    
-    return sorted(num_list)[5:]
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[len(arr) // 2]
+    left, equal, right = [], [], []
+    for num in arr:
+        if num < pivot:  # 오름차순 정렬
+            left.append(num)
+        elif num > pivot:
+            right.append(num)
+        else:
+            equal.append(num)
+    
+    return quick_sort(left) + equal + quick_sort(right)
+
+def solution(num_list):
+    sorted_list = quick_sort(num_list)
+    
+    return sorted_list[5:]
