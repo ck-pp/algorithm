b9e7b5889eaeb68d7e2c0b1c3e9829825ba530e3
ck <70942824+ck-pp@users.noreply.github.com>
2024-10-11 23:41:27 +0900

    [level 3] Title: 네트워크, Time: 1.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 0.53 ms
+메모리: 10.1 MB, 시간: 1.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 07일 23:28:22
+2024년 10월 11일 23:41:25
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,16 +1,24 @@
+from collections import deque
+
 def solution(n, computers):
-    def dfs(node, visited):
-        visited[node] = True
-        for neighbor in range(n):
-            if computers[node][neighbor] == 1 and visited[neighbor] == False:
-                dfs(neighbor, visited)
-        
     visited = [False] * n
     net_cnt = 0
     
+    def bfs(start):
+        q = deque([start])
+        visited[start] = True
+    
+        while q:
+            cur_node = q.popleft()
+            for i in range(n):
+                # 연결된 컴퓨터 중 방문하지 않은 컴퓨터 큐에 추가
+                if computers[cur_node][i] == 1 and not visited[i]:
+                    visited[i] = True
+                    q.append(i)
+    
     for i in range(n):
         if not visited[i]:
-            dfs(i, visited)
+            bfs(i)
             net_cnt += 1
-            
+    
     return net_cnt
