02c60ae831e26bb19a6e2088e985ee0b0e806def
ck <70942824+ck-pp@users.noreply.github.com>
2024-10-17 23:53:33 +0900

    [level 2] Title: 전화번호 목록, Time: 99.48 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,8 @@
 def solution(phone_book):
-    dict = {name:len(name) for name in phone_book}
-    dict = sorted(dict.items())
-    for i in range(len(dict)-1):
-        if dict[i][0] == dict[i+1][0][:dict[i][1]]:
+    # 한 번호가 다른 번호의 접두어인 경우 있는지 확인
+    phone_book.sort()
+    for i in range(len(phone_book) - 1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
             return False
+    
     return True
