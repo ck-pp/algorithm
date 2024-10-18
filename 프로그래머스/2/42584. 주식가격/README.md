d556d265973bd1f50b5c7c2da764236e4a2a1820
ck <70942824+ck-pp@users.noreply.github.com>
2024-10-18 23:56:06 +0900

    [level 2] Title: 주식가격, Time: 41.32 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 16.8 MB, 시간: 79.24 ms
+메모리: 19.4 MB, 시간: 41.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 09일 16:21:10
+2024년 10월 18일 23:56:03
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,15 +1,15 @@
-from collections import deque
-
 def solution(prices):
-    ans = deque([])
-    len_prices = len(prices)
-    for i in range(len_prices):
-        cnt = 0
-        for j in range(i+1, len_prices):
-            if prices[i] <= prices[j]:
-                cnt += 1
-            else:
-                cnt += 1
-                break
-        ans.append(cnt)
-    return list(ans)
+    # 초 단위 주식 가격 배열
+    # 가격이 떨어지지 않은 기간
+    stack = []
+    ans = [0 for _ in range(len(prices))]
+    for i in range(len(prices)):
+        while len(stack) > 0 and stack[-1][1] > prices[i]:
+            prev, _ = stack.pop()
+            ans[prev] = i - prev
+        stack.append((i, prices[i]))  # (현재 시점, 주식 가격)
+    
+    for i, price in stack:  # 스택에 값 남아있는 경우(가격이 떨어지지 않음)
+        ans[i] = len(prices) - i - 1
+        
+    return ans
