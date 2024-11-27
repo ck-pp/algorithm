8deb6e47065a86fd39ca2559bee0e6beddf872ea
ck <70942824+ck-pp@users.noreply.github.com>
2024-11-27 23:17:42 +0900

    [Silver II] Title: 촌수계산, Time: 52 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+num1, num2 = map(int, input().split())
+m = int(input())
+parent_childs = [tuple(map(int, input().split())) for _ in range(m)]
+
+g = [[] for _ in range(n+1)]
+for x, y in parent_childs:
+    g[x].append(y)  # x:부모 y:자식
+    g[y].append(x)
+
+def bfs(start, end):
+    q = deque([(0, start)])
+    visited = set([start])
+
+    while q:
+        steps, cur_num = q.popleft()
+    
+        if cur_num == end:
+            return steps
+    
+        for family_num in g[cur_num]:
+            if family_num <= end and family_num not in visited:  # 다음 넘버가 end 이하인지 체크하는 조건 중요!
+                q.append((steps + 1, family_num))
+                visited.add(family_num)
+    
+    return -1
+    
+print(bfs(num1, num2))
