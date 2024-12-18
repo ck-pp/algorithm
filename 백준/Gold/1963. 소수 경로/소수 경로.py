ffdc763b3814d38780af293a8cb074c00b89b8af
ck <70942824+ck-pp@users.noreply.github.com>
2024-12-18 23:24:24 +0900

    [Gold IV] Title: 소수 경로, Time: 112 ms, Memory: 34968 KB -BaekjoonHub

---
+++ 백준/Gold/1963. 소수 경로/소수 경로.py
@@ -0,0 +1,55 @@
+import sys
+from collections import deque
+imput = sys.stdin.readline
+
+def get_primes():
+    primes = [True] * 10000  # 네자리수로 제한되어 있으므로
+    primes[0] = primes[1] = False
+    for i in range(2, int(10000 ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, 10000, i):  # 에라토스테네스의 체의 원리: 소수의 배수는 소수가 아님.
+                primes[j] = False
+                
+    return primes
+    
+def generate_next_nums(cur_num, primes):
+    cur_num = str(cur_num)
+    next_nums = []
+    
+    for i in range(4):
+        for digit in "0123456789":
+            if i == 0 and digit == '0':  # 첫번째 자리는 1-9 가능
+                continue
+            if cur_num[i] == digit:  # 변환 안했을때
+                continue
+            
+            new_num = int(cur_num[:i] + digit + cur_num[i+1:])
+            
+            # 네 자리 소수인 숫자만 후보에 추가
+            if primes[new_num]:
+                next_nums.append(str(new_num))
+                
+    return next_nums
+
+T = int(input())
+primes = get_primes()
+for _ in range(T):
+    start, end = input().split()
+    q = deque([(0, start)])  # (변환 횟수, 숫자)
+    visited = set([start])
+    
+    # 첫번째 자리수 != 0
+    while q:
+        steps, cur_num = q.popleft()
+        
+        if cur_num == end:
+            print(steps)
+            break
+        
+        for next_num in generate_next_nums(cur_num, primes):
+            if next_num not in visited:
+                visited.add(next_num)
+                q.append((steps + 1, next_num))
+        
+    else:
+        print("Impossible")
