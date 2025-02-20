36fce66156f07e963debd864b0fafe0b858945a4
ck <70942824+ck-pp@users.noreply.github.com>
2025-02-20 11:32:42 +0900

    [Bronze I] Title: 약수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.py
+++ 백준/Bronze/1037. 약수/약수.py
@@ -8,7 +8,4 @@ divisors = list(map(int, input().split()))  # N의 진짜 약수(1, N 제외)
 divisors.sort()
 
 # 2. N 출력
-if n % 2 == 0:  # 약수의 짝이 다 존재할 경우
-    print(divisors[0] * divisors[-1])
-else:  # 약수 중에 제곱수가 있을 경우
-    print(divisors[n//2] ** 2)
+print(divisors[0] * divisors[-1])
