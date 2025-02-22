c0379974d32ec1797d943345161506eaaac0f518
ck <70942824+ck-pp@users.noreply.github.com>
2025-02-22 09:34:39 +0900

    [level 2] Title: 숫자의 표현, Time: 2.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 5.29 ms
+메모리: 10.3 MB, 시간: 2.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 15일 22:14:32
+2025년 02월 22일 09:34:36
 
 ### 문제 설명
 
--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,14 +1,17 @@
+from collections import deque
+
 def solution(n):
     ans = 0
-    i = 1
-    while i <= n:
-        sum = 0
-        for j in range(i, n+1):
-            sum += j
-            if sum == n:
-                ans += 1
-            elif sum > n:
-                sum = 0
-                break
-        i += 1
+    q = deque([])  # 연속된 자연수 담을 큐(FIFO)
+    sum = 0  # 현재 연속된 자연수 합
+    
+    for i in range(1, n + 1):
+        q.append(i)
+        sum += i
+        if sum > n:  # 현재 연속된 자연수 합이 n보다 클 경우
+            while sum > n:  # 작거나 같아질 때까지 작은 수부터 -
+                sum -= q.popleft()
+        if sum == n:  # 연속된 자연수 합이 n일 경우
+            ans += 1    
+    
     return ans
