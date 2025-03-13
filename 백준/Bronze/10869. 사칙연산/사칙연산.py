45a698949a3d48c8dfcb24c1dfb0eeacd87bc7fa
ck <70942824+ck-pp@users.noreply.github.com>
2025-03-14 02:35:42 +0900

    [Bronze V] Title: 사칙연산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+A, B = map(int, input().split())
+
+# 합 출력
+print(A + B)
+# 차 출력
+print(A - B)
+# 곱 출력
+print(A * B)
+# 몫 출력
+print(A // B)
+# 나머지 출력
+print(A % B)
