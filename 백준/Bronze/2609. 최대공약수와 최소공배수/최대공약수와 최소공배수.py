858e17ccc79cbd8171fb55450c66721a5ab423ec
ck <70942824+ck-pp@users.noreply.github.com>
2025-03-26 04:11:01 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,10 +1,20 @@
 import sys
-import math
 input = sys.stdin.readline
 
 num1, num2 = map(int, input().split())
 
+# 1. 최대공약수 계산 함수 정의
+def gcd(a, b):
+    # 유클리드 알고리즘(호제법)
+    while b > 0:
+        a, b = b, a % b
+    return a
+    
+# 2. 최소공배수 계산 함수 정의
+def lcm(a, b):
+    return a * b // gcd(a, b) 
+
 # 1. 최대공약수 출력
-print(math.gcd(num1, num2))
+print(gcd(num1, num2))
 # 2. 최소공배수 출력
-print(math.lcm(num1, num2))
+print(lcm(num1, num2))
