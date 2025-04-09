27496a68fc8192dc78140815b1342a58337e93d3
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-04-09 23:11:56 +0900

    [level 2] Title: 숫자 변환하기, Time: 0.51 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,26 +1,29 @@
 from collections import deque
 
 def solution(x, y, n):
-    q = deque([(y, 0)])  # (값, 연산 횟수)
+    # 연산 범위 줄이기 위해 y -> x로 찾는다. (나머지 == 0으로 조건 체크)
+    q = deque([(y, 0)])  # (현재 값, 연산 횟수)
+    visited = set([y])
     
-    if y == x:
-        return 0
-    
+    # bfs 로직 실행 (최소 연산 횟수)
     while q:
-        cur_val, steps = q.popleft()
+        cur_num, steps = q.popleft()
         
-        if cur_val == x:
+        if cur_num == x:
             return steps
-        
-        val_list = []
-        if cur_val - n > 0:
-            val_list.append(cur_val - n)
-        if cur_val % 2 == 0:
-            val_list.append(cur_val // 2)
-        if cur_val % 3 == 0:
-            val_list.append(cur_val // 3)
-        
-        for next_val in val_list:
-            q.append((next_val, steps + 1))
-
+            
+        possible_nums = []
+        # 문제에서 주어진 조건을 여기서 체크
+        if cur_num - n > 0 :
+            possible_nums.append(cur_num - n)
+        if cur_num % 2 == 0 :
+            possible_nums.append(cur_num // 2)
+        if cur_num % 3 == 0:
+            possible_nums.append(cur_num // 3)
+            
+        for next_num in possible_nums:
+            if next_num not in visited :
+                q.append((next_num, steps + 1))
+                visited.add(next_num)
+                
     return -1
