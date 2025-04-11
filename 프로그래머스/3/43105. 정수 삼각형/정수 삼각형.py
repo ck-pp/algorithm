e2b000019c13649e88c9acbb96391e94a4111455
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-04-11 23:52:50 +0900

    [level 3] Title: 정수 삼각형, Time: 73.29 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,15 +1,27 @@
-import copy as c
 
 def solution(triangle):
-    dp = c.deepcopy(triangle)
-    # 왼쪽 대각선 아래: [row+1][col], 오른쪽 대각선 아래: [row+1][col+1]
-    for row in range(1, len(dp)):
-        for col in range(len(dp[row])):
-            if col == 0:  # 맨 왼쪽
-                dp[row][col] += dp[row-1][col]
-            elif col == len(dp[row]) - 1:  # 맨 오른쪽
-                dp[row][col] += dp[row-1][col-1]
-            else:  # 그 사이
-                dp[row][col] += max(dp[row-1][col-1], dp[row-1][col])
+    # dp 정의
+    # dp[i][j]: i번째 행 j번째 열까지 거쳐간 숫자(합)의 최댓값 
+    dp = [row for row in triangle]
     
-    return max(dp[-1])
+    # 행 길이
+    n = len(triangle)
+    
+    for i in range(1, n):
+        # 열 길이
+        m = len(triangle[i])
+        for j in range(len(triangle[i])):
+            
+            # 1. 각 행의 왼쪽 끝자리 수일 경우
+            if j == 0:
+                dp[i][j] += dp[i-1][j]
+                
+            # 2. 각 행의 오른쪽 끝자리 수일 경우
+            elif j == m-1:
+                dp[i][j] += dp[i-1][j-1]
+                
+            # 3. 각 행의 양 끝이 아닐 경우
+            else:
+                dp[i][j] += max(dp[i-1][j-1], dp[i-1][j])
+    
+    return max(dp[n-1])
