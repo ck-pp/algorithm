3a9816bbf081e7f7c7f7488d5d68ba031e981b4b
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-04-14 23:49:49 +0900

    [level 3] Title: 등굣길, Time: 2.76 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,19 +1,22 @@
 def solution(m, n, puddles):
-    dp = [[0] * (m + 1) for _ in range(n + 1)]
-    dp[1][1] = 1
+    # 최단경로x, 최단경로 개수 = dp 사용
+    # (1, 1) (m, n) -> (0, 0) (m-1, n-1)
+    # dp 정의
+    # dp[i][j]: (j, i) 좌표까지의 최단경로 개수
+    dp = [[0] * m for _ in range(n)]
+    dp[0][0] = 1
     
-    for puddle in puddles:
-        dp[puddle[1]][puddle[0]] = -1
-        
-    # 오른쪽, 아래쪽으로만 이동 가능
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            if dp[i][j] < 0:  # 물웅덩이
+    # 물 웅덩이 -1로 표시
+    for x, y in puddles:
+        dp[y-1][x-1] = -1
+    
+    # 오른쪽, 아래쪽으로만 이동
+    for i in range(n):
+        for j in range(m):
+            if dp[i][j] == -1:
                 dp[i][j] = 0
             else:
-                if i > 1:
-                    dp[i][j] += dp[i-1][j]
-                if j > 1:
-                    dp[i][j] += dp[i][j-1]
-                    
-    return dp[n][m] % 1000000007
+                if 0 < i: dp[i][j] += dp[i-1][j]
+                if 0 < j: dp[i][j] += dp[i][j-1]
+                
+    return dp[n-1][m-1] % 1000000007
