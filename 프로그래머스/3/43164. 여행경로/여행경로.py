38cf0826eda4dbb77682629d960511292c4da95d
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-04-17 23:52:55 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,18 +1,25 @@
+from collections import defaultdict
+
+# dfs 정의
 def dfs(g, node, route):
-    while node in g and g[node]:  # 그래프에 node가 있는지 확인해야 함. 안하면 KeyError 발생.
+    while g[node]:
         next_node = g[node].pop()
         dfs(g, next_node, route)
     
     route.append(node)
-
+    
 def solution(tickets):
-    route = []  # 스택
-    tickets.sort(key=lambda x: x[1], reverse=True)  # 알파벳 순서 앞서는 경로 우선하기 위해
-    g = {start: [] for start, _ in tickets}
+    graph = defaultdict(list)
+    # pop은 오른쪽부터이므로 내림차순 정렬
+    tickets.sort(key=lambda city:city[1], reverse=True)
     
-    for start, end in tickets:
-        g[start].append(end)
+    # 경로 그래프 저장
+    for u, v in tickets:
+        graph[u].append(v)
+
+    route = []
+    # dfs 로직 실행
+    dfs(graph, "ICN", route)
     
-    dfs(g, "ICN", route)
-    
-    return route[::-1]  # 역으로 출력 (재귀 호출이므로)
+    # 재귀 호출 -> 역으로 출력
+    return route[::-1]
