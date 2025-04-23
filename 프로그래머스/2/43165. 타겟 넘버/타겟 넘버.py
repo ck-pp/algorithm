f33422a7fce8ea7b4c0d2dd2029983cd5a86a6d2
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-04-23 23:13:29 +0900

    [level 2] Title: 타겟 넘버, Time: 3.43 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,19 +1,18 @@
 from collections import Counter
 
 def solution(numbers, target):
-    # 가능한 모든 해를 찾아야 함 = DP로 풀이해보기
-    n = len(numbers)
-            
-    # 1. dp 정의
-    # dp[i]: i번째 수까지 더하거나 빼서 만들 수 있는 숫자 리스트
-    dp = [[] for _ in range(n)]
-    dp[0].extend([numbers[0], -numbers[0]])
-    
-    # 2중 for문인게 조금 걸리긴 한다... O((n-1)*2^n)
-    # 메모리 사용도!
-    for i in range(1, n):
-        for num in dp[i-1]:
-            dp[i].extend([num - numbers[i], num + numbers[i]])
-    
-    # 모든 정수들을 사용해 더하거나 빼서 target 넘버를 만들 수 있는 경우의 수
-    return Counter(dp[n-1])[target]
+    # dp: i-1까지 봤을 때 '합'별 경우의 수
+    dp = Counter()
+    dp[numbers[0]] += 1
+    dp[-numbers[0]] += 1
+
+    for num in numbers[1:]:
+        # dp 자체 값을 갱신해버리면, 반복문 실행 중에 값이 바뀌어버린다.
+        # 반복 도중 딕셔너리(dict)를 변경하면 RuntimeError 발생
+        next_dp = Counter()
+        for sum_v, cnt in dp.items():
+            next_dp[sum_v + num] += cnt
+            next_dp[sum_v - num] += cnt
+        dp = next_dp
+
+    return dp[target]
