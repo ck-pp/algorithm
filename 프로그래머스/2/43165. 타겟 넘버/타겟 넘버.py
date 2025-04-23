f0cdb83505e856d3e133725f19c8e43ee9994932
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-04-23 22:45:23 +0900

    [level 2] Title: 타겟 넘버, Time: 289.54 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,19 @@
+from collections import Counter
+
 def solution(numbers, target):
+    # 가능한 모든 해를 찾아야 함 = DP로 풀이해보기
     n = len(numbers)
+            
+    # 1. dp 정의
+    # dp[i]: i번째 수까지 더하거나 빼서 만들 수 있는 숫자 리스트
+    dp = [[] for _ in range(n)]
+    dp[0].extend([numbers[0], -numbers[0]])
     
-    # 1. dfs 정의
-    def dfs(idx, sum_val):
-        # 모든 정수를 연산에 사용한 경우
-        if idx == n:
-            # 타겟 넘버가 만들어지면 방법의 수 + 1
-            return 1 if sum_val == target else 0
-        
-        # 다음 숫자를 더하고 빼서 재귀 호출
-        return dfs(idx+1, sum_val + numbers[idx]) + dfs(idx+1, sum_val - numbers[idx])
+    # 2중 for문인게 조금 걸리긴 한다... O((n-1)*2^n)
+    # 메모리 사용도!
+    for i in range(1, n):
+        for num in dp[i-1]:
+            dp[i].extend([num - numbers[i], num + numbers[i]])
     
-    # 2. dfs 호출
-    return dfs(0, 0)
+    # 모든 정수들을 사용해 더하거나 빼서 target 넘버를 만들 수 있는 경우의 수
+    return Counter(dp[n-1])[target]
