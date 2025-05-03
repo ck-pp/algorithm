b06718484380e692a7b29daefd80dc639a11508c
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-05-03 23:53:55 +0900

    [Bronze III] Title: 팩토리얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -2,9 +2,10 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-factorial = 1
+facto = 1
 
 for i in range(2, N+1):
-    factorial *= i
+    facto *= i
 
-print(factorial)
+print(facto)
+    
