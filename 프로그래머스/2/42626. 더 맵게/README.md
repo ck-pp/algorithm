678dfb1a1f2297a55de1b9fee27f36449c615cd2
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-05-20 22:54:21 +0900

    [level 2] Title: 더 맵게, Time: 1714.99 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3) 
 
 ### 성능 요약
 
-메모리: 50.8 MB, 시간: 2757.03 ms
+메모리: 50.8 MB, 시간: 1714.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 19일 22:41:16
+2025년 05월 20일 22:54:17
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,21 +1,19 @@
 import heapq as hq
 
 def solution(scoville, K):
-    # 모든 음식 지수 K 이상될 때 까지
-    # 가장 안 매운 음식 지수 + 2번째 안 매운 음식 지수 * 2
+    ans = 0    
+    # 기본 min-heap
+    hq.heapify(scoville)
     
-    hq.heapify(scoville)  # 최소 힙(min-heap)
-    steps = 0  # 섞어야 하는 최소 횟수
-    
+    # 가장 작은 스코빌 지수가 K보다 작을 동안 반복
+    # = 모든 스코빌 지수가 K 이상이 아닐 동안 반복
     while scoville[0] < K:
         # 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우
         if len(scoville) < 2:
             return -1
         
-        # 섞은 음식의 스코빌 지수
-        new_scoville = hq.heappop(scoville) + hq.heappop(scoville) * 2
-        hq.heappush(scoville, new_scoville)
+        new = hq.heappop(scoville) + hq.heappop(scoville) * 2
+        hq.heappush(scoville, new)
+        ans += 1
         
-        steps += 1
-        
-    return steps
+    return ans
