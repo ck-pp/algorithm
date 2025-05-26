307741fe13af217d65bee651338090ef659785e6
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-05-26 23:17:44 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,4 +1,10 @@
+def isSquareNumber(num):
+    # 제곱수일 경우
+    if num ** (1/2) - int(num ** (1/2)) == 0:
+        return (int(num ** (1/2)) + 1) ** 2
+    # 제곱수가 아닐 경우
+    else:
+        return -1
+    
 def solution(n):
-    if n ** (1/2) - int(n ** (1/2)) == 0:
-        return (n ** (1/2) +1) ** 2
-    return -1
+    return isSquareNumber(n)
