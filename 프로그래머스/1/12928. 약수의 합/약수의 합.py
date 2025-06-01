fc63c049e81e9ab0c14281bbbd598bfad09df5e6
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-06-01 23:45:12 +0900

    [level 1] Title: 약수의 합, Time: 0.15 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,8 +1,2 @@
 def solution(n):
-    ans = 0
-    for i in range(1, int(n**(1/2))+1):
-        if n % i == 0:
-            ans += i
-            if i != n//i:
-                ans += (n//i)
-    return ans
+    return sum([i for i in range(1, n + 1) if n % i == 0])
