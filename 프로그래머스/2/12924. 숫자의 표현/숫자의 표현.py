5732ec558be5398084d3e1bc8783f630073268ab
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-06-13 23:38:03 +0900

    [level 2] Title: 숫자의 표현, Time: 1.90 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,19 +1,19 @@
 from collections import deque
 
 def solution(n):
-    ans = 0
-    q = deque([])
-    sum_q = 0
-    
-    for i in range(1, n + 1):
-        q.append(i)
-        sum_q += i
+    dq = deque([])
+    cur_sum, ans = 0, 0
+    for i in range(1, n+1):
+        # 값 추가
+        dq.append(i)
+        cur_sum += i
         
-        if sum_q > n:
-            while sum_q > n:
-                sum_q -= q.popleft()
-                
-        if sum_q == n:
+        # 연속한 자연수들의 합이 n보다 큰 경우
+        while cur_sum > n:
+            cur_sum -= dq.popleft()
+        
+        ## 연속한 자연수들의 합이 n인 경우 카운트
+        if cur_sum == n:
             ans += 1
-                
+            
     return ans
