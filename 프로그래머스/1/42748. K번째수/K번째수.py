502713c2ed058fe4d0c360fcadb5ad5ddd58a522
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-06-19 23:46:07 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,24 +1,3 @@
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    
-    pivot = arr[len(arr) // 2]
-    left, equal, right = [], [], []
-    
-    for num in arr:  # 오름차순 정렬
-        if num < pivot:
-            left.append(num)
-        elif num > pivot:
-            right.append(num)
-        else:
-            equal.append(num)
-    
-    return quick_sort(left) + equal + quick_sort(right)
-
 def solution(array, commands):
-    ans = []
-    for i, j, k in commands:
-        sorted_arr = quick_sort(array[i-1:j])
-        ans.append(sorted_arr[k-1])
-        
-    return ans
+    
+    return list(sorted(array[i-1:j])[k-1] for i, j, k in commands)
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.02 ms
+메모리: 9.21 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 13일 22:30:02
+2025년 06월 19일 23:46:04
 
 ### 문제 설명
 
