857015f16af9bb3f061de53c7cdfbbbe4b42cb46
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-06-29 23:26:55 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994?language=python3) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.08 ms
+메모리: 9.27 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 03일 21:11:10
+2025년 06월 29일 23:26:52
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def solution(cards1, cards2, goal):
+    ans = "Yes"
+    cards1 = deque(cards1)
+    cards2 = deque(cards2)
+    
+    for i in range(len(goal)):
+        word = goal[i]
+        
+        # 카드1 뭉치에서 뽑은 카드가 원하는 단어인 경우
+        if cards1 and cards1[0] == word:
+            cards1.popleft()
+        # 카드2 뭉치에서 뽑은 카드가 원하는 단어인 경우
+        elif cards2 and cards2[0] == word:
+            cards2.popleft()
+        # 모든 카드 뭉치에 원하는 단어가 뽑히지 않은 경우
+        else:
+            ans = "No"
+            break
+            
+    return ans
