fcfe1cf3963e4316b63cfaef7fe0662e6ee640dd
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-06-30 23:46:46 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.03 ms
+메모리: 9.15 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 21일 16:05:29
+2025년 06월 30일 23:46:45
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,14 +1,14 @@
 def solution(n, words):
-    used_word = set([words[0]])
-    ans = [0, 0]
-    for i in range(1, len(words)):
-        cur_word = words[i]
-        if words[i-1][-1] != cur_word[0] or cur_word in used_word:
-            ans[0] = (i % n) + 1  # 탈락자 번호
-            ans[1] = (i // n) + 1  # 탈락자 차례
-            break
-
-        # 중복 체크 위해 사용된 단어 리스트에 현재 단어 추가
-        used_word.add(cur_word)
-
-    return ans
+    turns = 0  # 현재 턴 수
+    used_words = []
+    for w in words:
+        # 1) 이미 나온 단어이거나 
+        # 2) 앞사람이 말한 단어의 마지막 문자로 시작하는 단어가 아닌 경우
+        if w in used_words or (used_words and used_words[-1][-1] != w[0]):
+            return (turns % n + 1, turns // n + 1)
+        
+        used_words.append(w)
+        turns += 1
+        
+    return [0, 0]
+        
