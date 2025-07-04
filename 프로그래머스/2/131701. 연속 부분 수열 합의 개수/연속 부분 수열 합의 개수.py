3abb8ef4711d4f8371aab9c5e1f55bda45b3ff9b
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-07-04 23:01:53 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 3699.81 ms, Memory: 42.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,9 +1,14 @@
 def solution(elements):
-    ans = []
+    sums = set(elements)  # 초기화(길이 1인 부분 수열 합)
+    
+    n = len(elements)
+    # 최대 한바퀴이므로 배열을 2번 반복한다. (= 메모리 성능 이슈)
     elements.extend(elements)
-    for i in range(len(elements)//2):
-        sum_a = elements[i]
-        for j in range(1, len(elements)//2+1):
-            ans.append(sum_a)
-            sum_a += elements[i+j]
-    return len(set(ans))
+    for i in range(2, n):
+        for j in range(n):
+            sums.add(sum(elements[j:j+i]))
+            
+    sums.add(sum(elements))  # 길이 n인 부분 수열 = 수열 자체 합은 1번만 구해서 더하도록 함
+    
+    return len(sums)
+    
