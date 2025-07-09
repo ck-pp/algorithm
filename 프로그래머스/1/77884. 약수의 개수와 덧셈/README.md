9f76304c9387d340a3c55c87a68afa0c11acc93d
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-07-09 22:13:38 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 1.60 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 개수와 덧셈 - 77884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884) 
 
 ### 성능 요약
 
-메모리: 9.27 MB, 시간: 1.52 ms
+메모리: 9.31 MB, 시간: 1.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 23일 23:58:02
+2025년 07월 09일 22:13:36
 
 ### 문제 설명
 
--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,22 +1,15 @@
-# 약수 개수가 짝수인지 판별해주는 함수 (T/F)
-def isFactorEven(num):
-    factor_cnt = 0
+def factorNum(num):  # 약수 개수 반환 함수
+    cnt = 0
     for i in range(1, int(num ** (1/2)) + 1):
-        if num % i == 0:
-            factor_cnt += 2
-            # 제곱수인 경우 약수 1개가 1쌍
-            if i == num // i:
-                factor_cnt -= 1
-            
-    return factor_cnt % 2 == 0
+        if num % i == 0:  # 약수
+            if num // i != i:  # 제곱수가 아닌 경우
+                cnt += 2
+            else:  # 제곱수인 경우
+                cnt += 1
+    
+    return cnt % 2 == 0
 
 def solution(left, right):
-    ans = 0
-    for n in range(left, right + 1):
-        if isFactorEven(n):
-            ans += n
-        else:
-            ans -= n
-            
-    return ans
+    # 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 빼기
+    return sum(n if factorNum(n) else -n for n in range(left, right+1))
         
