ee43a5981b81f310f8dff8bd76bb9eea57090033
chae-kyeong <70942824+ck-pp@users.noreply.github.com>
2025-07-17 23:23:37 +0900

    [level 1] Title: 예산, Time: 0.03 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,12 +1,11 @@
 def solution(d, budget):
     d.sort()  # 신청 금액별 오름차순 정렬
     
-    now, total_money = 0, 0
-    for money in d:  # 모든 부서에 지원 가능한 경우
+    total_money = 0
+    for i in range(len(d)):  # 모든 부서에 지원 가능한 경우
         # 지원금이 예산을 넘어가기 전에 종료
-        if total_money + money > budget:
-            break
-        total_money += money
-        now += 1
+        if total_money + d[i] > budget:
+            return i
+        total_money += d[i]
     
-    return now
+    return i + 1
